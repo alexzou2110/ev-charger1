a1d429f25c557d7e63434b3ab297d05a33e7610c
alexzou2110 <alexzou2110@gmail.com>
2025-09-30 17:34:22 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'ev-charger-v1';
+const OFFLINE_FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
+});
